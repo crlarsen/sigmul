cc87550455b504a9aacaac1c278f2dc1b5080ebf
Chris Larsen <larsencr@gmail.com>
2021-03-10 14:23:21 -0800

    Use my own logic to perform addition.

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Code is explained in the video series [Building an FPU in Verilog](https://www.y
 See the video *Building an FPU in Verilog: Building an 11-bit Multiply*.
 
 This version of the code:
-- Builds the fourth version of the multiply circuit. It relies on Verilog to generate adders for each of the partial products.
-- Minimize the number of bits which need to be added at each stage.
+- Builds the fifth version of the multiply circuit.
+- It no longer relies on Verilog to generate adders for each of the partial products.
 
 ## Manifest
 
--- sigmul.v
+++ sigmul.v
@@ -3,18 +3,16 @@
 // Copyright:  2021
 // Engineer: Chris Larsen
 // 
-// Create Date: 03/09/2021 03:38:00 PM
+// Create Date: 03/09/2021 03:50:00 PM
 // Design Name: 
 // Module Name: sigmul
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description: Fourth version of the significand multiply logic. The code
-//              depends on Verilog to generate adder logic. This version of
-//              the code should be slightly faster than the previous version
-//              because reduces the numbers of bits which need to be added at
-//              each iteration from 2*NSIG+2 to just NSIG+1 creating an NSIG+2
-//              bit wider result.
+// Description: Fifth version of the significand multiply logic. The code no
+//              longer depends on Verilog to generate adder logic. This
+//              version is probably slower than the previous version. This
+//              loss of speed is to be expected at this stage.
 // 
 // Dependencies: 
 // 
@@ -24,6 +22,42 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module hadder(a, b, s, c);
+  input a, b;
+  output s, c;
+
+  assign s = a ^ b;
+  assign c = a & b;
+endmodule
+
+module fadder(a, b, Cin, s, Cout);
+  input a, b, Cin;
+  output s, Cout;
+
+  assign s = a ^ b ^ Cin;
+  assign Cout = (a & b) | (a & Cin) | (b & Cin);
+endmodule
+
+module adder(a, b, s);
+  parameter NSIG = 10;
+  input [NSIG:0] a, b;
+  output [NSIG+1:0] s;
+
+  wire [NSIG:1] c;
+
+  hadder H(a[0], b[0], s[0], c[1]);
+
+  genvar i;
+  generate
+    for (i = 1; i < NSIG; i = i + 1)
+      begin
+        fadder Fi(a[i], b[i], c[i], s[i], c[i+1]);
+      end
+  endgenerate
+
+  fadder Ffinal(a[NSIG], b[NSIG], c[NSIG], s[NSIG], s[NSIG+1]);
+endmodule
+
 module sigmul(a, b, p);
   parameter NSIG = 10;
 
@@ -31,16 +65,16 @@ module sigmul(a, b, p);
   output [2*NSIG+1:0] p;
 
   wire [NSIG:0] rt[NSIG-1:0]; // Running total of partial products
-  
+
   assign {rt[0], p[0]} = {NSIG+1{b[0]}} & a;
-  
+
   genvar i;
   generate
     for (i = 1; i < NSIG; i = i + 1)
     begin
-      assign {rt[i], p[i]} = rt[i-1] + ({NSIG+1{b[i]}} & a);
+      adder #(NSIG) si(rt[i-1], ({NSIG+1{b[i]}} & a), {rt[i], p[i]});
     end
   endgenerate
-  
-  assign p[2*NSIG+1:NSIG] = rt[NSIG-1] + ({NSIG+1{b[NSIG]}} & a);
+
+  adder #(NSIG) sFinal(rt[NSIG-1], ({NSIG+1{b[NSIG]}} & a), p[2*NSIG+1:NSIG]);
 endmodule
